f03b4ff0b777bdcdb41f3d35b0c1cbc0854a4774
elif <elifbiyik.00@gmail.com>
2024-01-11 09:17:25 +0300

    Commit

--- app/src/main/java/com/ex/github/ui/MainActivity.kt
+++ app/src/main/java/com/ex/github/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ex.github.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import com.ex.github.R
@@ -16,11 +18,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: SignInViewModel by viewModels()
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val window = window
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = resources.getColor(R.color.black)
+        }
 
         var useruid = viewModel.getCurrentUserUID()
 
@@ -29,12 +38,10 @@ class MainActivity : AppCompatActivity() {
             var editor = sharedPreferences.edit()
             editor.putString("uid", useruid).apply()
 
-            // Splash ekran
             Handler(Looper.getMainLooper()).postDelayed({
                 loadFragment(HomePageFragment())
             }, 2000)
         } else
-        // Splash ekran
             Handler(Looper.getMainLooper()).postDelayed({
                 loadFragment(SignInFragment())
             }, 2000)
